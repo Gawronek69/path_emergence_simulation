e19917cea77238682a9a2d4657d4bda90c37e355
sggorski <sggorski@interia.pl>
2025-10-23 12:41:36 +0200

    Added spawning agents at entrance positions

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ from mesa.discrete_space import OrthogonalMooreGrid
 
 from environment import TestEnvironment
 from agent import ParkAgent
+from utils import entrances
 
 class ParkModel(mesa.Model):
     def __init__(self, num_agents=5, width=100, height=100, seed = 42):
@@ -10,25 +11,31 @@ class ParkModel(mesa.Model):
         self.num_agents = num_agents
         self.grid = OrthogonalMooreGrid((width, height), torus=False, random=self.random)
         self.environment = TestEnvironment(width, height)
+        self.step_count = 0
+        self.spawn_cells = None
 
     def setup(self):
         terrain, obstacles = self.environment.create()
         self.grid.add_property_layer(terrain)
         self.grid.add_property_layer(obstacles)
-        self.create_agents()
-
-    def create_agents(self):
-        sidewalk_cells = [
-            cell for cell in self.grid.all_cells.cells
-            if getattr(cell, "sidewalk", False)
+        self.spawn_cells = [
+            cell
+            for cell in self.grid.all_cells
+            if cell.coordinate in entrances.doria_pamphil
         ]
+        self.spawn_agents(3)
 
+
+    def spawn_agents(self, num_agents):
         ParkAgent.create_agents(
             model=self,
-            n=self.num_agents,
-            cell=self.random.sample(sidewalk_cells, k=self.num_agents),
+            n=num_agents,
+            cell=self.random.sample(self.spawn_cells, k=num_agents),
         )
 
 
     def step(self):
+        self.step_count += 1
+        if self.step_count % 10 == 0:
+            self.spawn_agents(3)
         self.agents.shuffle_do("step")
