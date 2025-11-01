5df0ae067d8b28a4c84fca80be298445b6e2d1c7
pavlvs-91 <gleindekp@gmail.com>
2025-11-01 22:50:54 +0100

    London Blackheath Park grid

--- model.py
+++ model.py
@@ -27,7 +27,7 @@ class ParkModel(mesa.Model):
         self.spawn_cells = [
             cell
             for cell in self.grid.all_cells
-            if cell.coordinate in entrances.greenwich
+            if cell.coordinate in entrances.blackheath
         ]
         self.spawn_agents(3)
 
--- utils/images.py
+++ utils/images.py
@@ -9,7 +9,8 @@ from utils.terrains import Terrain
 def get_coordinates(image):
     images = {"doria_pamphil": "utils/park_imgs/doria_pamphil.png",
               "doria_pamphil_west": "utils/park_imgs/doria_pamphil_west.png",
-              "greenwich": "utils/park_imgs/greenwich.png",}
+              "greenwich": "utils/park_imgs/greenwich.png",
+              "blackheath": "utils/park_imgs/blackheath.png",}
 
     try:
         path = images[image]
