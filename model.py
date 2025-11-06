d1fe0a9790fe072b21d066bf4fe2b2d67c5a3649
pavlvs-91 <gleindekp@gmail.com>
2025-11-06 18:51:54 +0100

    Park grids improvements

--- model.py
+++ model.py
@@ -44,7 +44,7 @@ class ParkModel(mesa.Model):
         self.spawn_cells = [
             cell
             for cell in self.grid.all_cells
-            if cell.coordinate in entrances.hampstead
+            if cell.coordinate in entrances.greenwich
         ]
         self.spawn_agents(3)
 
